bc403406db9b85643de32c305c5043188acab0dc
Jorge Moreno <alterebro@gmail.com>
2016-04-22 17:51:06 +0100

    app preloader

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -14,7 +14,6 @@ var months_es = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio',
 var weekdays_es = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
 
 var vd = {
-
 	items : null,
 	regions : null,
 	months : null,
@@ -29,6 +28,7 @@ var vd = {
 	],
 	current_item : {},
 	filtered_items : null,
+	loading : true
 }
 
 var vm = new Vue({
@@ -45,30 +45,6 @@ var vm = new Vue({
 		es_month : function(v) {
 			return months_es[v-1].toUpperCase().substr(0, 3);
 		},
-
-		/*
-		distanceFilter : function(arr, segment) {
-
-			var segment = segment.split(',');
-			if (segment.length < 2 && !segment[0]) { return arr; }
-
-			n_from = parseInt(segment[0]);
-			n_to = parseInt(segment[1]) || 1000;
-
-			var aux = [];
-			for ( var i=0; i<arr.length; i++) {
-				var d = arr[i].distance;
-				if (
-					isNumeric(d)
-					&& d >= n_from
-					&& d <= n_to
-				) {
-					aux.push(arr[i]);
-				}
-			}
-			return aux;
-		},
-		*/
 		distanceValue : function(v) {
 			var val = '';
 			for (var i=0; i<this.distances.length; i++) {
@@ -78,37 +54,17 @@ var vm = new Vue({
 			}
 			return val;
 		},
-
-		/*
-		count: function (arr) {
-			var res = arr.length;
-			return parseInt(res);
-		},
-		empty: function(arr) {
-			return (arr.length == 0) ? true : '';
-		},
-		*/
-
-		date_w : function(w) {
-			return weekdays_es[parseInt(w)-1];
-		},
-		date_m : function(m) {
-			return months_es[parseInt(m)-1];
-		},
-		date_y : function(y) {
-			return '20' + y;
-		},
-
+		date_w : function(w) { return weekdays_es[parseInt(w)-1]; },
+		date_m : function(m) { return months_es[parseInt(m)-1]; },
+		date_y : function(y) { return '20' + y; },
 		distance_num : function(d) {
 			return isNumeric(d) ? d : '';
 		},
-
 		month_yymm : function(yymm) {
 			var m = months_es[ parseInt(yymm.substr(2,2)) -1 ];
 			var y = '20' + yymm.substr(0,2);
 			return m + ' ' + y;
 		}
-
 	},
 	// ---------------------------
 	computed: {
@@ -121,7 +77,6 @@ var vm = new Vue({
 			output.sort();
 			return output;
 		},
-
 		months : function() {
 			var output = [];
 			for ( var i=0; i<this.items.length; i++ ) {
@@ -130,22 +85,18 @@ var vm = new Vue({
 			}
 			return output;
 		},
-
 		filtered_items : function() {
-
 			var check_dist = function(origin, target) {
 				var segment = target.split(',');
 				if (segment.length < 2 && !segment[0]) { return false; }
 
 				n_from = parseInt(segment[0]);
 				n_to = parseInt(segment[1]) || 1000;
-
 				return ( isNumeric(origin) && origin >= n_from && origin <= n_to ) ? true : false;
 			}
 
 			var output = [];
 			for ( var i=0; i<this.items.length; i++ ) {
-
 				var remove = false;
 				var item = this.items[i];
 				if ( this.selected_region != '' && item['region'] != this.selected_region ) { remove = true; }
@@ -171,10 +122,10 @@ var vm = new Vue({
 
 						var h = window.location.hash.slice(3) || false;
 						self.enroute(h);
+						self.loading = false;
 
 					} else {
 						// Error
-						// TODO: handle error
 					}
 				}
 			};
@@ -184,11 +135,15 @@ var vm = new Vue({
 		enroute: function(slug) {
 
 			this.get_item(slug);
-
 		},
-
+		do_reset : function() {
+			this.enroute(false);
+			this.selected_region = '';
+			this.selected_distance = '';
+			this.selected_date = '';
+			this.getJSON();
+		},
 		get_item : function(slug) {
-
 			var out = {};
 			for (var i = 0; i < this.items.length; i++) {
 				if (this.items[i].slug === slug) {
